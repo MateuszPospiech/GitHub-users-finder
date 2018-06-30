c0f550c24f4d342afedbaf79b3a9f679595e3491
MateuszPospiech <pospiechmateusz@gmail.com>
2018-06-30 17:05:26 +0200

    Add some styles and simple fat arrow function to test button

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
-var person = {
-    name: 'Mateo',
-    subname: 'Pośpiech',
-    age: '26'
-};
-console.log(person.name);
+const seachButton = document.querySelector(".seachButton");
+
+
+const github_ID = "Iv1.2ee4419151d8a568";
+const github_secret_ID = "daa0c2b24791d4459f9a2d04f433c42227b1df65";
+
+
+seachButton.addEventListener("click", () => {
+    console.log("click");
+})
