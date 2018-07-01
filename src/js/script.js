d98e3189c67c1e30e7972fd923a6a512cfbd7b60
MateuszPospiech <pospiechmateusz@gmail.com>
2018-07-01 21:45:42 +0200

    it works!

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,13 @@
 const seachButton = document.querySelector(".seachButton");
 const inputValue = document.querySelector("#search");
 
+const profileName = document.querySelector(".main__profile-name");
+const profileUserName = document.querySelector(".main__profile-username");
+const profileRepos = document.querySelector(".main__profile-repos");
+const profileUrl = document.querySelector(".main__profile-url");
+const profileFollowers = document.querySelector(".main__profile-followers");
+const profileFollowing = document.querySelector(".main__profile-following");
+
 
 const github_ID = "Iv1.2ee4419151d8a568";
 const github_secret_ID = "daa0c2b24791d4459f9a2d04f433c42227b1df65";
@@ -21,6 +28,13 @@ const showData = () => {
     //promise
     fetchUsers(inputValue.value).then((res) =>{
         console.log(res);   
+        
+        profileName.innerHTML = `Name: ${res.data.name}`;
+        profileUserName.innerHTML = `Login: ${res.data.login}`;
+        profileRepos.innerHTML = `Public repos: ${res.data.public_repos}`;
+        profileUrl.innerHTML = `URL profile: ${res.data.html_url}`;
+        profileFollowers.innerHTML = `Followers: ${res.data.followers}`;
+        profileFollowing.innerHTML = `Following: ${res.data.following}`;
     })
 };
 
